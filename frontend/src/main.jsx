7b254a1dd8e46f699b78add93bbdca040879b832
Wild <zoppoz4100@gmail.com>
2025-12-20 16:26:24 +0400

    style: added vars, global styles; changed main
    - Added dark theme and light theme variables
    - Imported styles to main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import "./styles/reset.css";
+import "./styles/variables.css";
 import "./styles/global.css";
 
 import App from "./App.jsx";
